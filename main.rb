43af14bc40030fd5b13076466d541a69e6ec934a
achoo-o <amyj.etana@gmail.com>
2024-11-23 16:17:10 +1300

    put separate functions into module

--- main.rb
+++ main.rb
@@ -3,32 +3,36 @@ player = "X"
 opponent = "O"
 board = [Array.new(3, " "), Array.new(3, " "), Array.new(3, " ")]
 
-def ShowBoard(board)
-  3.times do |i|
-    print " "
-    print board[i][0]
-    print " | "
-    print board[i][1]
-    print " | "
-    print board[i][2]
-    print "\n"
-    if i < 2 then
-      print "-----------"
+module Game
+  def show_board(board)
+    3.times do |i|
+      print " "
+      print board[i][0]
+      print " | "
+      print board[i][1]
+      print " | "
+      print board[i][2]
       print "\n"
+      if i < 2 then
+        print "-----------"
+        print "\n"
+      end
+    end
+  end
+
+  def play()
+    while player_move = gets.chomp
+      if player_move == "pussy" #check
+        break
+      else
+        puts "Please write a valid move in the following format: 0, 1 OR 0,1"
+        puts "REMINDER: 0,0 is the top left position, and 2,2 is the bottom right."
+      end
     end
   end
 end
 
-def Play()
-  while player_move = gets.chomp
-    if player_move == "pussy" #check
-      break
-    else
-      puts "Please write a valid move in the following format: 0, 1 OR 0,1"
-      puts "REMINDER: 0,0 is the top left position, and 2,2 is the bottom right."
-    end
-  end
-end
+include Game
 
-ShowBoard(board)
-Play()
+Game.show_board(board)
+Game.play()
