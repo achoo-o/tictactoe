570df9f775f7b6d8cde36844a1722e757ea6173c
achoo-o <amyj.etana@gmail.com>
2024-11-23 19:19:41 +1300

    refactor and remove method

--- lib/player.rb
+++ lib/player.rb
@@ -5,15 +5,11 @@ class Player
 
   def choose_marker()
     puts ('Do you want to be naughts (O) or crosses (X)?')
-    while marker = gets.chomp.downcase
-      if self.marker_valid?(marker) then break end
+    while input = gets.chomp.downcase
+      if ['naughts', 'crosses', 'x', 'o'].include? input then break end
       puts ("Please type 'o', 'x, 'naughts', or 'crosses'.")
     end
     marker
-  end
-
-  def marker_valid?(marker)
-    ['naughts', 'crosses', 'x', 'o'].include? marker
   end
 
   attr_accessor :marker
