da569a278ebed1d6949c19b34e8ed4f10279f258
achoo-o <amyj.etana@gmail.com>
2024-11-23 16:09:55 +1300

    Create a semi-working method of user input

--- main.rb
+++ main.rb
@@ -19,4 +19,17 @@ def ShowBoard(board)
   end
 end
 
+def Play()
+  while player_move = gets.chomp
+    if player_move == "pussy" #check
+      break
+    else
+      puts "Please write a valid move in the following format:"
+      puts "0, 1 OR 0,1"
+      puts "...Where 0,0 is the top left position, and 2,2 is the bottom right."
+    end
+  end
+end
+
 ShowBoard(board)
+Play()
