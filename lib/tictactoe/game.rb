f876db004a67016570fe693dad4c79413b22eada
achoo-o <amyj.etana@gmail.com>
2024-11-24 13:48:35 +1300

    add show board method

--- lib/tictactoe/game.rb
+++ lib/tictactoe/game.rb
@@ -5,12 +5,26 @@ class Game
 
   def play(player_one, player_two)
     self.print_header("LET'S PLAY!", 4)
+    while !self.is_game_over?(@board)
+      self.show_board(@board)
+    end
   end
 
   def print_header(str, padding = 2)
     line = "#" * (str.length + padding * 2)
     header = " " * padding + str +  " " * padding
     puts "\n#{line}\n#{header}\n#{line}\n"
+  end
+
+  def is_game_over?(board)
+    false
+  end
+
+  def show_board(board)
+    3.times do |i|
+      puts " #{board[i][0]} | #{board[i][1]} | #{board[i][2]} \n"
+      if i < 2 then print "-----------\n" end
+    end
   end
   #attr_accessor
 end
