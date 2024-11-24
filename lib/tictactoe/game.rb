3b5cf4e787b22a20860c12ed419753282dea5bbf
achoo-o <amyj.etana@gmail.com>
2024-11-24 13:53:28 +1300

    add user input section

--- lib/tictactoe/game.rb
+++ lib/tictactoe/game.rb
@@ -1,12 +1,15 @@
 class Game
   def initialize()
     @board = [Array.new(3, " "), Array.new(3, " "), Array.new(3, " ")]
+    @curr_player = 1
   end
 
   def play(player_one, player_two)
     self.print_header("LET'S PLAY!", 4)
     while !self.is_game_over?(@board)
       self.show_board(@board)
+      puts "Player #{@curr_player}, it's your turn."
+      make_a_move(gets.chomp.strip)
     end
   end
 
@@ -25,6 +28,10 @@ class Game
       puts " #{board[i][0]} | #{board[i][1]} | #{board[i][2]} \n"
       if i < 2 then print "-----------\n" end
     end
+  end
+
+  def make_a_move(coordinates)
+    puts "Move made"
   end
   #attr_accessor
 end
