ee90232d906be8aa611aded646391a38537377db
achoo-o <amyj.etana@gmail.com>
2024-11-26 20:25:13 +1300

    remove commented out code

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-#First, just write a game in Ruby! Work in what you can, otherwise just start with spaghetti nonsense. We can clean up later.
 require './lib/tictactoe_play'
 
 player_one = Player.new
@@ -6,48 +5,3 @@ puts ("Thanks! Player #1, you've chosen #{player_one.marker_name}.")
 player_two = Player.new(player_one.marker)
 puts ("Player #2, you'll be #{player_two.marker_name}.")
 Game.new.play([player_one, player_two])
-# player = "X"
-# opponent = "O"
-# board = [Array.new(3, " "), Array.new(3, " "), Array.new(3, " ")]
-
-# module Game
-#   def play()
-    
-#   end
-
-#   def show_board(board)
-#     3.times do |i|
-#       print " "
-#       print board[i][0]
-#       print " | "
-#       print board[i][1]
-#       print " | "
-#       print board[i][2]
-#       print "\n"
-#       if i < 2 then
-#         print "-----------"
-#         print "\n"
-#       end
-#     end
-#   end
-
-#   def make_a_move()
-#     while player_move = gets.chomp
-#       if player_move.match(/^[0-2],[0-2]$/) 
-#         puts "Valid"
-#         break
-#       else
-#         puts "Please write a valid move in the following format: 0, 1 OR 0,1"
-#         puts "REMINDER: 0,0 is the top left position, and 2,2 is the bottom right."
-#       end
-#     end
-#   end
-# end
-
-# class Player
-#   include Game
-# end
-
-# player = Player.new()
-# player.show_board(board)
-# player.make_a_move()
